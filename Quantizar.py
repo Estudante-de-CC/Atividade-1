0e6330954a72ab83646408361c114502b389cb19
Estudante-de-CC <estudanteiniciante71@gmail.com>
2025-06-27 08:09:34 -0300

    Função para quantização da imagem manual
    Refiz a função de quantização para executar a operação manualmente. Tudo completamente manual. LEGAL!

--- Quantizar.py
+++ Quantizar.py
@@ -1,50 +1,68 @@
-import cv2
+import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
+from math import floor
 
 def carregar(): 
-    #Testei com 3 imagens diferentes também. Funcionou. 
-    imagem = cv2.imread("cores.jpeg")
-    altura, largura, nhe = imagem.shape
-    #Redimensionar para o tamanho da tela
-    imagem = cv2.resize(imagem, (int(largura/2), int(altura/2)))
-    #imagem = cv2.cvtColor(imagem, cv2.COLOR_RGB2GRAY)
+    #Abrir imagem
+    imagem = cv.imread("baixados.jpeg")
+    imagem = cv.cvtColor(imagem, cv.COLOR_RGB2GRAY)
+    altura, largura = imagem.shape
+
+    #Redimensionar
+    n_largura, n_altura = (int(largura/2), int(altura/2))
+    imagem = cv.resize(imagem, (0,0), fx=0.469, fy=0.469)
 
     return imagem
 
 def quantizar(entrada, k):
+    #recriar o processo de quantização do 0
+    altura, largura = entrada.shape
 
-    #Essa função está bem parecida com o presente da demonstração. Eu usei a de lá como exemplo pra essa parte
-    #Estava tentando entender como a função cv2.kmeans funcionava... 
-    D = entrada.reshape((-1,3))
-    
-    D = np.float32(D)
-    
-    criterios = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 5, 1.0)
-    K = k
-    ret,label,centro=cv2.kmeans(D,K,None,criterios,2,cv2.KMEANS_RANDOM_CENTERS)
-    
-    center = np.uint8(centro)
-    res = center[label.flatten()]
-    res2 = res.reshape((entrada.shape))
+    #Tudo em padrões de escalas de cinza. -> Essa eu fiz na mão...
+    quantizada = entrada.copy()
+    valor = round(255/(k))
+    for i in range(0, altura): 
+        for j in range(0, largura): 
+            quanta = floor(entrada[i, j]/valor)*valor
+            quantizada[i, j] = quanta
 
-    return res2
+    return quantizada
 
 def apresentar_cv2(entrada, k):
-    cv2.namedWindow(f"VASH! Quantizado {k}")
-    cv2.imshow(f"VASH! Quantizado {k}", entrada)
-    cv2.waitKey(0)
+    cv.imshow(f"Quantizado {k}", entrada)
+    cv.waitKey(0)
+
+def exibir_juntas(imagem, quantizadores):
+    superior = quantizar(imagem, quantizadores[0])
+    inferior = quantizar(imagem, quantizadores[floor(len(quantizadores)/2)])
+    for i in range (1, len(quantizadores)):
+        if i < len(quantizadores)/2:
+            superior = np.concatenate((superior, quantizar(imagem, quantizadores[i])), axis=1)
+        elif i > len(quantizadores)/2:
+            inferior = np.concatenate((inferior, quantizar(imagem, quantizadores[i])), axis=1)
+
+    superior = np.concatenate((superior , inferior), axis = 0)
+
+    apresentar_cv2(superior, "Total")
+    cv.destroyAllWindows()
+
+def exibir_separadamente(imagem, quantizadores):
+    for i in quantizadores: 
+        exibir = quantizar(imagem, i)
+        apresentar_cv2(exibir, i)
+    cv.destroyAllWindows()
 
 def main():
     imagem = carregar()
     quantizadores = [128, 64, 32, 16, 8, 2]
-    j = 0  
-    for i in quantizadores:
-        exibir = quantizar(imagem, i)
-        apresentar_cv2(exibir, i)
+    altura, largura = imagem.shape
+
+    #exibir individualnmente: 
+    exibir_separadamente(imagem, quantizadores)
+
+    #Exige ajustar a imagem às dimensões de tela.
+    exibir_juntas(imagem, quantizadores)
     
-    cv2.destroyAllWindows()
 
 if __name__ == "__main__": 
     main()
-
